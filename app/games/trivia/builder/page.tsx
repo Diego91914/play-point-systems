ee40070448e0215a7261cec5d9b7ff0f6bfbfa3b
Diego91914 <channing.stovall@gmail.com>
2026-08-29 10:39:31 -0500

    Add full trivia rules guide to host builder

--- app/games/trivia/builder/page.tsx
+++ app/games/trivia/builder/page.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { ProductShell } from "../../../components/ProductShell";
+import { RulesCorner } from "../../_components/RulesCorner";
 import { TriviaLiveBuilderExperience } from "../play/TriviaLiveBuilderExperience";
 
 export const metadata: Metadata = {
@@ -27,6 +28,7 @@ export default function TriviaBuilderPage() {
       ]}
     >
       <TriviaLiveBuilderExperience />
+      <RulesCorner game="trivia" />
     </ProductShell>
   );
 }
