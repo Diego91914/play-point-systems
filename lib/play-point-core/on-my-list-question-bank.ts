cdc03f30d824b77d10447595428c78f59ac46168
Diego91914 <channing.stovall@gmail.com>
2026-08-31 11:01:22 -0500

    Include restored On My List questions

--- lib/play-point-core/on-my-list-question-bank.ts
+++ lib/play-point-core/on-my-list-question-bank.ts
@@ -1,11 +1,13 @@
 import { ON_MY_LIST_QUESTIONS as BASE_QUESTIONS } from "@/lib/play-point-core/on-my-list-questions";
 import { ON_MY_LIST_EXTRA_QUESTIONS } from "@/lib/play-point-core/on-my-list-questions-extra";
+import { ON_MY_LIST_RESTORED_QUESTIONS } from "@/lib/play-point-core/on-my-list-questions-restored";
 
 export type OnMyListQuestion = { id: string; prompt: string };
 
 export const ON_MY_LIST_QUESTIONS: readonly OnMyListQuestion[] = [
   ...BASE_QUESTIONS.map(({ id, prompt }) => ({ id, prompt })),
   ...ON_MY_LIST_EXTRA_QUESTIONS,
+  ...ON_MY_LIST_RESTORED_QUESTIONS,
 ];
 
 function normalizePrompt(prompt: string) {
